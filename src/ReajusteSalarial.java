f6e2e01826cae8a087b31b36ee4d56caf24980fc
Joyceolympio <markathere@hotmail.com>
2023-06-14 18:46:10 -0300

    :tada: commit teste

--- src/ReajusteSalarial.java
+++ src/ReajusteSalarial.java
@@ -5,10 +5,10 @@ public class ReajusteSalarial {
     public void reajusteSalarioFuncionario(Funcionario funcionario, Desempenho desempenho){
 
         if (desempenho == Desempenho.BOM){
-
             BigDecimal reajuste = funcionario.getSalario().multiply(new BigDecimal("0.10"));
             funcionario.reajusteSalario(reajuste);
-
+        }else if (desempenho == Desempenho.A_DESEJAR){
+            throw new IllegalArgumentException("Funcionário não pode receber reajuste, seu desempenho foi a desejar");
         }
 
     }
--- src/ReajusteSalarialTest.java
+++ src/ReajusteSalarialTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ReajusteSalarialTest {
 
@@ -16,8 +16,38 @@ public class ReajusteSalarialTest {
         reajusteSalarial.reajusteSalarioFuncionario(funcionario, Desempenho.BOM);
 
         assertEquals(new BigDecimal("1100.00"), funcionario.getSalario());
+    }
+
+    @Test
+    public void reajusteDeveriaSerZero(){
+
+        ReajusteSalarial reajusteSalarial = new ReajusteSalarial();
+        Funcionario funcionario = new Funcionario("Joy", new BigDecimal("1000.00"));
+//
+//        reajusteSalarial.reajusteSalarioFuncionario(funcionario, Desempenho.A_DESEJAR);
+//
+//        assertEquals(new BigDecimal("1000.00"), funcionario.getSalario());
+
+        assertThrows(IllegalArgumentException.class, () -> reajusteSalarial.reajusteSalarioFuncionario(funcionario, Desempenho.A_DESEJAR));
+
+//        try{
+//            reajusteSalarial.reajusteSalarioFuncionario(funcionario, Desempenho.A_DESEJAR);
+//            fail("Não deu exception!");
+//        } catch (Exception e){
+//
+//        }
+    }
+
+    @Test
+    public void reajusteDeveriaSerZero1(){
+
+        ReajusteSalarial reajusteSalarial = new ReajusteSalarial();
+        Funcionario funcionario = new Funcionario("Joy", new BigDecimal("1000.00"));
+
+        reajusteSalarial.reajusteSalarioFuncionario(funcionario, Desempenho.A_DESEJAR);
+
+        assertEquals(new BigDecimal("1000.00"), funcionario.getSalario());
 
 
     }
-
 }
